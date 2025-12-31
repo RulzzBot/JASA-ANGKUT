e2243e6dc5f116418bf83c50c706d756f2ea4c9d
YutaDev <nazrulwijaya0@gmail.com>
2025-12-31 17:36:54 +0700

    Update Keunggulan

--- script/global.js
+++ script/global.js
@@ -28,7 +28,7 @@ services.forEach(item => {
     Container.innerHTML += `
     <div class="p-6 bg-white shadow-md rounded-xl">
         <div class="h-32 mb-4 bg-gray-200 rounded">
-        // gambar disini
+        <!--gambar disini-->
         </div>
         <h3 class="font-semibold">${item.title}</h3>
         <p class="text-sm text-gray-600 mt-2">${item.desc}</p>
@@ -39,4 +39,40 @@ services.forEach(item => {
         </div>
     </div>
     `
+})
+
+const keunggulan  = [
+    {
+        title: "Armada Aman & Dimodifikasi",
+        icon : "fa-solid fa-truck-pickup",
+        desc : "Armada kami di modifikasi khusus untuk angkut barang agar lebih aman dan nyaman.",
+    },
+    {
+        title: "Driver Berpengalaman",
+        icon : "fa-solid fa-user-tie",
+        desc : "Supir sudah terbiasa Angut Barang Besar, Pindahan, dan perjalanan Jauh.",
+    },
+    {
+        title: "Harga Transparan dan Nego",
+        icon : "fa-solid fa-tags",
+        desc : "Harga Jelas dari Awal, bisa disesuaikan dengan jarak & Muatan",
+    },
+    {
+        title : "Cepat dan Tepat Waktu",
+        icon  : "fa-solid fa-clock",
+        desc : "Respon cepat Via WhatsApp, Proses pengangkutan Langsung Jalan.",
+    },
+]
+
+const ContainerKeunggulan = document.getElementById("keunggulan");
+
+keunggulan.forEach(unggul => {
+    ContainerKeunggulan.innerHTML += `
+    <div class="flex items-start gap-6 p-6 border rounded-xl">
+        <i class="${unggul.icon} mt-2 text-3xl text-orange-500"></i>
+        <div>
+            <h3 class="font-semibold">${unggul.title}</h3>
+            <p class="mt-2 text-sm text-gray-600">${unggul.desc}</p>
+        </div>
+    </div>`
 })
